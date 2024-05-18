b3445e8b8c9d102dec185e287a57d1fd70405865
Henderson The Coder School <68570781+hendersoncoderschool@users.noreply.github.com>
2024-05-18 11:59:27 -0700

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float turnSpeed;
+    public float maxSpeed;
+    private Rigidbody rb;       
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("w")){
+            //transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            rb.AddForce(transform.forward  * speed * Time.deltaTime);
+        }
+         if(Input.GetKey("s")){
+            transform.Translate(Vector3.forward * -speed * 0.5f * Time.deltaTime);
+        }
+         if(Input.GetKey("a")){
+            transform.Rotate(Vector3.up, -speed * Time.deltaTime);
+        }
+        if(Input.GetKey("d")){
+            transform.Rotate(Vector3.up * speed * Time.deltaTime);
+        }
+
+    }
+}
